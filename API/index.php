8f6586adfe7a2814db647dfd7c3be453f9ee0225
Ronald Lobo <ronald.lobo@pointclearsolutions.com>
2016-07-25 23:23:41 -0600

    CORS

--- API/index.php
+++ API/index.php
@@ -10,7 +10,7 @@ require '../API/Vendor/JWT/SignatureInvalidException.php';
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header("Access-Control-Allow-Origin: localhost:4200");
+    header("Access-Control-Allow-Origin: *");
     header('Access-Control-Allow-Credentials: true');    
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
 }   
